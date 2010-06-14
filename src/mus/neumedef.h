8c421ab649655fc791c5010df64a2e0a28ac79b9
Chris Niven <cniven@3b0cfccb-802f-0410-bfa2-860b70618a2d>
2010-06-14 15:48:54 +0000

    adding neumedef.h

---
+++ src/mus/neumedef.h
@@ -0,0 +1,35 @@
+/*
+ *  neumedef.h
+ *  aruspix
+ *
+ *  Created by Chris Niven on 10/06/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* Festa Dies A */
+
+#define nPODATUS 39 // podatus (epiphonus)
+#define nBEMOLE 46	// b flat
+#define nVIRGULA 59 // comma
+#define nCEPHALICUS 76 
+#define nPES 77		// bottom of podatus
+#define nDIAMOND 83
+#define nAPOSTROPHE 84
+#define nQUILISMA 93 // squiggly
+#define nPORRECT_4 112 // 4 steps down porrectus
+#define nPUNCTUM 115 // square punctum
+#define nGUIONE 116 // 'next pitch' indicator 
+#define nPORRECT_1 126 // one step down porrectus
+#define nPORRECT_2 170 // two steps down porrectus
+#define nFCLEF 182 // f clef
+#define nCCLEF 186 // c clef
+#define nDIAMOND_SMALL 194 //punctum inclinatum parvum (diamond)
+#define nPORRECT_3 218 // 3 steps down porrectus
+#define nNATURAL 227 // becuadros (natural sign)
+#define nWHITEPUNCT 402 // nota excavata (white punctum)
+
+#define PUNCT_PADDING 25 // space between punctums in open editing mode
+#define PUNCT_WIDTH 5 // 'guesstimation' of punctum width -- this number is fairly arbitrary
+
+
