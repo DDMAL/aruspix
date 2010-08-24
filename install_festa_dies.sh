e82b772d24514b76460b1cc44d0f5b6751947a71
Chris Niven <cniven@3b0cfccb-802f-0410-bfa2-860b70618a2d>
2010-08-24 19:37:07 +0000

    edited scripts

--- install_festa_dies.sh
+++ install_festa_dies.sh
@@ -3,11 +3,12 @@
 #a handy script to install festa dies after font editing
 # chris niven 2010
 
-cp -v ~/aruspix/Festa_Dies_Font/FestaDiesA.dfont ~/aruspix/trunk/osx/install_root/Library/Fonts/FestaDiesA.dfont
-cp -v ~/aruspix/Festa_Dies_Font/FestaDiesB.dfont ~/aruspix/trunk/osx/install_root/Library/Fonts/FestaDiesB.dfont
-cp -v ~/aruspix/Festa_Dies_Font/FestaDiesA.dfont ~/aruspix/trunk/varia/font/FestaDiesA.dfont
-cp -v ~/aruspix/Festa_Dies_Font/FestaDiesB.dfont ~/aruspix/trunk/varia/font/FestaDiesB.dfont
-cp -v ~/aruspix/Festa_Dies_Font/Festa\ Dies\ A.ttf ~/aruspix/trunk/varia/font/FestaDiesA.ttf
-cp -v ~/aruspix/Festa_Dies_Font/Festa\ Dies\ B.ttf ~/aruspix/trunk/varia/font/FestaDiesB.ttf
-cp -v ~/aruspix/Festa_Dies_Font/FestaDiesA.dfont ~/Library/Fonts/FestaDiesA.dfont
-cp -v ~/aruspix/Festa_Dies_Font/FestaDiesB.dfont ~/Library/Fonts/FestaDiesB.dfont
+#cp -v ~/aruspix/Festa_Dies_Font/FestaDiesA.dfont ~/aruspix/trunk/osx/install_root/Library/Fonts/FestaDiesA.dfont
+#cp -v ~/aruspix/Festa_Dies_Font/FestaDiesB.dfont ~/aruspix/trunk/osx/install_root/Library/Fonts/FestaDiesB.dfont
+#cp -v ~/aruspix/Festa_Dies_Font/FestaDiesA.dfont ~/aruspix/trunk/varia/font/FestaDiesA.dfont
+#cp -v ~/aruspix/Festa_Dies_Font/FestaDiesB.dfont ~/aruspix/trunk/varia/font/FestaDiesB.dfont
+#cp -v ~/aruspix/Festa_Dies_Font/Festa\ Dies\ A.ttf ~/aruspix/trunk/varia/font/FestaDiesA.ttf
+#cp -v ~/aruspix/Festa_Dies_Font/Festa\ Dies\ B.ttf ~/aruspix/trunk/varia/font/FestaDiesB.ttf
+#cp -v ~/aruspix/Festa_Dies_Font/FestaDiesB.dfont ~/Library/Fonts/FestaDiesB.dfont
+cp -v ~/aruspix/trunk/varia/font/FestaDiesA.dfont ~/Library/Fonts/FestaDiesA.dfont
+cp -v ~/aruspix/trunk/varia/font/FestaDiesA.dfont ~/aruspix/trunk/osx/install_root/Library/Fonts/FestaDiesA.dfont
