56c0d2dacf7c92ef709950c9311c9a17e1545dd8
Alastair Porter <alastair.porter@mail.mcgill.ca>
2011-03-29 18:52:52 +0000

    Add unit test support for neumes

---
+++ test/mus/musneume_test.cpp
@@ -0,0 +1,19 @@
+/*
+ *  musneume_test.cpp
+ *  aruspix
+ *
+ *  Created by Alastair Porter on 11-03-29.
+ *  Copyright 2011 com.aruspix.www. All rights reserved.
+ *
+ */
+
+
+#include <gtest/gtest.h>
+#include "musneume.h"
+
+TEST(NeumeTest, TestXyz) {
+	
+	
+	ASSERT_EQ("document1", "document2");
+	
+}
