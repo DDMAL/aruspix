9b50c092800b929116841acd0faf2078b496ff96
Laurent Pugin <lxpugin@gmail.com>
2012-09-18 15:50:19 +0200

    version 2.0.0

--- src/app/axapp.cpp
+++ src/app/axapp.cpp
@@ -27,7 +27,7 @@ const wxString IPC_START = "StartOther";
 int AxApp::s_version_major = 2;
 int AxApp::s_version_minor = 0;
 int AxApp::s_version_revision = 0;
-wxString AxApp::s_version = wxString::Format("%d.%d.%d beta", AxApp::s_version_major, AxApp::s_version_minor, AxApp::s_version_revision);
+wxString AxApp::s_version = wxString::Format("%d.%d.%d", AxApp::s_version_major, AxApp::s_version_minor, AxApp::s_version_revision);
 wxString AxApp::s_build_date = __DATE__;
 wxString AxApp::s_build_time = __TIME__;
 
