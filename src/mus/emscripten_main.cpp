fab249eb6514357d17cb1f5732f3908bec389757
Rodolfo Zitellini <rwz@xhero.org>
2013-11-06 17:36:54 +0100

    JSON parsing for testing

--- src/mus/emscripten_main.cpp
+++ src/mus/emscripten_main.cpp
@@ -19,20 +19,46 @@ using namespace jsonxx;
 
 
 extern "C" {
-    const char * convertMusic(ConvertFileType input_format, ConvertFileType output_format, const char * c_data) {
+    const char * convertMusic(const char * c_notation, const char * c_options) {
         
-        string data(c_data);
+        string notation(c_notation);
+        string json_options(c_options);
         string out_str;
         
+        string in_format;
+        string out_format;
+        int scale = 100;
+        int border = 10;
+        
+        Object json;
+        
+        // Read JSON options
+        json.parse(json_options);
+        
+        if (json.has<String>("InputFormat"))
+            in_format = json.get<String>("InputFormat");
+        
+        if (json.has<String>("OutputFormat"))
+            out_format = json.get<String>("OutputFormat");
+        
+        if (json.has<Number>("Scale"))
+            scale = json.get<Number>("Scale");
+        
+        if (json.has<Number>("Border"))
+            border = json.get<Number>("Border");
+        
         MusController controller;
+        
+        controller.SetScale(scale);
+        controller.SetBorder(border);
         
         Mus::SetResourcesPath("/data");
         
         // default to mei if unset.
-        if (input_format == pae_file) {
-            controller.LoadString( data, pae_file );
+        if (in_format == "pae") {
+            controller.LoadString( notation, pae_file );
         } else {
-            controller.LoadString( data, mei_file );
+            controller.LoadString( notation, mei_file );
         }
         
         // in the future we will be able to render to mei too
